efc44b9bea1df2256150ba9a15c8263af337da17
TrifanBogdan24 <trifanbogdan24@gmail.com>
2024-05-04 19:31:15 +0300

    one-liner queries

--- src/main/scala/Queries.scala
+++ src/main/scala/Queries.scala
@@ -1,41 +1,34 @@
+import scala.util.Try
+
+
 object Queries {
 
+  def killJackSparrow(t: Table): Option[Table] =
+    queryT(Some(FilterRows(t, Field("name", _ != "Jack"))))
 
-  def killJackSparrow(t: Table): Option[Table] = {
-    val condition: FilterCond = Field("name", name => name != "Jack")
-    val filteredData = t.filter(condition)
-    Some(filteredData)
-  }
-
-  def insertLinesThenSort(db: Database): Option[Table] = {
-    // Creăm tabela "Inserted Fellas" și inserăm datele specificate
-    val insertedTable = Table("Inserted Fellas", List(
-      Map("name" -> "Ana", "age" -> "93", "CNP" -> "455550555"),
-      Map("name" -> "Diana", "age" -> "33", "CNP" -> "255532142"),
-      Map("name" -> "Tatiana", "age" -> "55", "CNP" -> "655532132"),
-      Map("name" -> "Rosmaria", "age" -> "12", "CNP" -> "855532172")
-    ))
-
-    // Sortăm tabela după vârstă și returnăm rezultatul
-    Some(insertedTable.sort("age"))
-  }
+  def insertLinesThenSort(db: Database): Option[Table] = InsertRow(Table("Inserted Fellas", List.empty), List(
+    Map("name" -> "Ana", "age" -> "93", "CNP" -> "455550555"),
+    Map("name" -> "Diana", "age" -> "33", "CNP" -> "255532142"),
+    Map("name" -> "Tatiana", "age" -> "55", "CNP" -> "655532132"),
+    Map("name" -> "Rosmaria", "age" -> "12", "CNP" -> "855532172")
+  )).eval.map(_.sort("age"))
 
 
-  def youngAdultHobbiesJ(db: Database): Option[Table] = {
-    val peopleTable = db.tables.find(_.tableName == "People")
-    val hobbiesTable = db.tables.find(_.tableName == "Hobbies")
 
-    (peopleTable, hobbiesTable) match {
-      case (Some(people), Some(hobbies)) =>
-        val filteredPeople = people.tableData.filter(row => row("name").startsWith("J") && row("age").toInt < 25)
-        val joinedData = filteredPeople.flatMap { person =>
-          val personName = person("name")
-          val matchingHobbies = hobbies.tableData.filter(_("name") == personName)
-          matchingHobbies.map(hobby => Map("name" -> personName, "hobby" -> hobby("hobby")))
-        }
-        Some(Table("YoungAdultHobbies", joinedData))
-      case _ => None
-    }
-  }
+  def youngAdultHobbiesJ(db: Database): Option[Table] =
+    db.tables.find(_.tableName == "People")
+      .flatMap(peopleTable =>
+        db.tables.find(_.tableName == "Hobbies")
+          .map(hobbiesTable =>
+            Table("YoungAdultHobbies", peopleTable.tableData
+              .filter(row => row("name").startsWith("J") && row("age").toInt < 25)
+              .flatMap(person =>
+                hobbiesTable.tableData.filter(_("name") == person("name"))
+                  .map(hobby => Map("name" -> person("name"), "hobby" -> hobby("hobby")))
+              )
+            )
+          )
+      )
+
 
 }
